85171b85e37b71a31f9f1a6726a98cc12c73d58a
Bjarte Stien Karlsen <bjarte@bjartek.org>
2022-07-12 14:18:17 +0200

    added more tests

--- lib/go/test/metadata_test.go
+++ lib/go/test/metadata_test.go
@@ -2,241 +2,222 @@ package test
 
 import (
 	"encoding/json"
-	"fmt"
 	"testing"
 
+	. "github.com/bjartek/overflow/overflow"
 	"github.com/onflow/cadence"
-	jsoncdc "github.com/onflow/cadence/encoding/json"
-	"github.com/onflow/flow-go-sdk"
-	"github.com/onflow/flow-go-sdk/crypto"
 	"github.com/stretchr/testify/assert"
-
-	"github.com/onflow/flow-nft/lib/go/templates"
 )
 
 func TestSetupRoyaltyReceiver(t *testing.T) {
-	b, accountKeys := newTestSetup(t)
 
-	exampleNFTAccountKey, exampleNFTSigner := accountKeys.NewWithSigner()
-	_, metadataAddress, exampleNFTAddress := deployNFTContracts(t, b, exampleNFTAccountKey)
+	o, err := OverflowTesting(WithBasePath("../../.."), WithScriptFolderName("transactions/scripts"))
+	assert.NoError(t, err)
+
+	setupRoyalty := o.TxFileNameFN("setup_account_to_receive_royalty")
+	setupAccount := o.TxFileNameFN("setup_account")
 
 	t.Run("Should not be able to setup a royalty receiver for a vault that doesn't exist", func(t *testing.T) {
-
-		// This transaction creates a new public link to the specified storage path
-		// at a public path determined by the `MetadataViews.getRoyaltyReceiverPublicPath()` function
-		// It can be used for any fungible token the beneficiary wants to receive
-		script := templates.GenerateSetupAccountToReceiveRoyaltyScript(metadataAddress, flow.HexToAddress(emulatorFTAddress))
-		tx := createTxWithTemplateAndAuthorizer(b, script, exampleNFTAddress)
-
-		// Here we use a storage path that points to nothing, so it will fail
-		// The positive case is handled in the `mintExampleNFT()` test case
-		vaultPath := cadence.Path{Domain: "storage", Identifier: "missingVault"}
-		tx.AddArgument(vaultPath)
-
-		signAndSubmit(
-			t, b, tx,
-			[]flow.Address{
-				b.ServiceKey().Address,
-				exampleNFTAddress,
-			},
-			[]crypto.Signer{
-				b.ServiceKey().Signer(),
-				exampleNFTSigner,
-			},
-			true,
-		)
+		setupAccount(SignProposeAndPayAs("alice")).AssertSuccess(t)
+		setupRoyalty(SignProposeAndPayAs("alice"), Arg("vaultPath", "/storage/missingVault")).
+			AssertFailure(t, "A vault for the specified fungible token path does not exist")
 	})
 }
 
 func TestGetNFTMetadata(t *testing.T) {
-	b, accountKeys := newTestSetup(t)
+	o, err := OverflowTesting(WithBasePath("../../.."), WithScriptFolderName("transactions/scripts"))
+	assert.NoError(t, err)
 
-	// Create new keys for the NFT contract account
-	// and deploy all the NFT contracts
-	exampleNFTAccountKey, exampleNFTSigner := accountKeys.NewWithSigner()
-	nftAddress, metadataAddress, exampleNFTAddress := deployNFTContracts(t, b, exampleNFTAccountKey)
+	setupAccount := o.TxFileNameFN("setup_account")
+	setupFlowRoyalty := o.TxFileNameFN("setup_account_to_receive_royalty", Arg("vaultPath", "/storage/flowTokenVault"))
+	setupAccount(SignProposeAndPayAs("alice")).AssertSuccess(t)
+	setupFlowRoyalty(SignProposeAndPayAs("alice")).AssertSuccess(t)
 
-	// Mint a single NFT with standard royalty cuts and metadata
-	mintExampleNFT(t, b,
-		accountKeys,
-		nftAddress, metadataAddress, exampleNFTAddress,
-		exampleNFTAccountKey,
-		exampleNFTSigner)
+	setupAccount(SignProposeAndPayAs("bob")).AssertSuccess(t)
+	setupFlowRoyalty(SignProposeAndPayAs("bob")).AssertSuccess(t)
+
+	mintNft := o.TxFileNameFN("mint_nft", SignProposeAndPayAsServiceAccount(),
+		Arg("name", "Example NFT 0"),
+		Arg("description", "This is an example NFT"),
+		Arg("thumbnail", "example.jpeg"),
+		Arg("cuts", "[0.25, 0.40]"),
+		Arg("royaltyDescriptions", `["minter","creator"]`),
+		Addresses("royaltyBeneficiaries", "alice", "bob"))
+
+	id := mintNft(Arg("recipient", "alice")).AssertSuccess(t).GetIdFromEvent("A.f8d6e0586b0a20c7.ExampleNFT.Deposit", "id")
 
 	t.Run("Should be able to verify the metadata of the minted NFT through the Display View", func(t *testing.T) {
 
-		// Run a script to get the Display view for the specified NFT ID
-		script := templates.GenerateGetNFTMetadataScript(nftAddress, exampleNFTAddress, metadataAddress)
-		result := executeScriptAndCheck(
-			t, b,
-			script,
-			[][]byte{
-				jsoncdc.MustEncode(cadence.NewAddress(exampleNFTAddress)),
-				jsoncdc.MustEncode(cadence.NewUInt64(0)),
-			},
-		)
+		var metadata Metadata
+		err := o.Script("get_nft_metadata", Arg("address", "alice"), Arg("id", id)).MarhalAs(&metadata)
+		assert.NoError(t, err)
 
-		// Expected metadata
-		const (
-			name        = "Example NFT 0"
-			description = "This is an example NFT"
-			thumbnail   = "example.jpeg"
-			externalURL = "https://example-nft.onflow.org/0"
-		)
+		//Here we could just assert on the values of the return type aswell if we want to
+		for id, trait := range metadata.Traits.Traits {
+			if trait.Name == "mintedTime" {
+				metadata.Traits.Traits[id].Value = "mock_time"
+			}
+		}
+		data, err := json.MarshalIndent(metadata, "", " ")
+		assert.NoError(t, err)
 
-		nftResult := result.(cadence.Struct)
-
-		nftType := fmt.Sprintf("A.%s.ExampleNFT.NFT", exampleNFTAddress)
-
-		assert.Equal(t, cadence.String(name), nftResult.Fields[0])
-		assert.Equal(t, cadence.String(description), nftResult.Fields[1])
-		assert.Equal(t, cadence.String(thumbnail), nftResult.Fields[2])
-		assert.Equal(t, cadence.NewAddress(exampleNFTAddress), nftResult.Fields[3])
-		assert.Equal(t, cadence.String(nftType), nftResult.Fields[4])
-
-		// TODO: To verify the return data from the script with the expected data.
-		royalties := toJson(t, nftResult.Fields[5])
-		// Declared an empty interface of type Array
-		var results map[string]interface{}
-
-		// Unmarshal or Decode the JSON to the interface.
-		json.Unmarshal([]byte(royalties), &results)
-
-		// Verify external URL view result is as expected
-		assert.Equal(t, cadence.String(externalURL), nftResult.Fields[6])
-
-		// Assert that the serial number is correct
-		assert.Equal(t, cadence.NewUInt64(0), nftResult.Fields[7])
-
-		// Verify NFTCollectionData results are as expected
-		const (
-			pathName                = "exampleNFTCollection"
-			collectionType          = "A.f3fcd2c1a78f5eee.ExampleNFT.Collection"
-			collectionPublicType    = "A.f3fcd2c1a78f5eee.ExampleNFT.ExampleNFTCollectionPublic"
-			nftCollectionPublicType = "A.01cf0e2f2f715450.NonFungibleToken.CollectionPublic"
-			nftReceiverType         = "A.01cf0e2f2f715450.NonFungibleToken.Receiver"
-			resolverCollectionType  = "A.179b6b1cb6755e31.MetadataViews.ResolverCollection"
-			providerType            = "A.01cf0e2f2f715450.NonFungibleToken.Provider"
-		)
-		assert.Equal(t, cadence.Path{Domain: "public", Identifier: pathName}, nftResult.Fields[8])
-		assert.Equal(t, cadence.Path{Domain: "storage", Identifier: pathName}, nftResult.Fields[9])
-		assert.Equal(t, cadence.Path{Domain: "private", Identifier: pathName}, nftResult.Fields[10])
-		assert.Equal(t, cadence.String(fmt.Sprintf("&%s{%s}", collectionType, collectionPublicType)), nftResult.Fields[11])
-		assert.Equal(t, cadence.String(fmt.Sprintf("&%s{%s,%s,%s,%s}", collectionType, collectionPublicType, nftCollectionPublicType, nftReceiverType, resolverCollectionType)), nftResult.Fields[12])
-		assert.Equal(t, cadence.String(fmt.Sprintf("&%s{%s,%s,%s,%s}", collectionType, collectionPublicType, nftCollectionPublicType, providerType, resolverCollectionType)), nftResult.Fields[13])
-
-		// Verify NFTCollectionDisplay results are as expected
-		const (
-			collectionName        = "The Example Collection"
-			collectionDescription = "This collection is used as an example to help you develop your next Flow NFT."
-			collectionImage       = "https://assets.website-files.com/5f6294c0c7a8cdd643b1c820/5f6294c0c7a8cda55cb1c936_Flow_Wordmark.svg"
-			collectionExternalURL = "https://example-nft.onflow.org"
-		)
-		assert.Equal(t, cadence.String(collectionName), nftResult.Fields[14])
-		assert.Equal(t, cadence.String(collectionDescription), nftResult.Fields[15])
-		assert.Equal(t, cadence.String(collectionExternalURL), nftResult.Fields[16])
-		assert.Equal(t, cadence.String(collectionImage), nftResult.Fields[17])
-		assert.Equal(t, cadence.String(collectionImage), nftResult.Fields[18])
-
-		// TODO: Verify `nftResult.Fields[19]` is equal to a {String: String} dictionary
-		// with key `twitter` and value `https://twitter.com/flow_blockchain`
-
-		// Verify Edition results are as expected
-		const (
-			editionName = "Example NFT Edition"
-			editionNum  = 0
-		)
-		expectedName, _ := cadence.NewString(editionName)
-		assert.Equal(t, cadence.NewOptional(expectedName), nftResult.Fields[20].(cadence.Struct).Fields[0])
-		assert.Equal(t, cadence.NewUInt64(editionNum), nftResult.Fields[20].(cadence.Struct).Fields[1])
-		assert.Equal(t, cadence.NewOptional(nil), nftResult.Fields[20].(cadence.Struct).Fields[2])
-
-		minterName, _ := cadence.NewString("minter")
-
-		traitsView := nftResult.Fields[21].(cadence.Struct)
-		traits := traitsView.Fields[0].(cadence.Array)
-
-		mintTrait := traits.Values[0].(cadence.Struct)
-		assert.Equal(t, minterName, mintTrait.Fields[0])
-		assert.Equal(t, fmt.Sprintf("0x%s", exampleNFTAddress.String()), mintTrait.Fields[1].String())
-		assert.Equal(t, cadence.NewOptional(nil), mintTrait.Fields[2])
-		assert.Equal(t, cadence.NewOptional(nil), mintTrait.Fields[3])
-
-		blockNumberName, _ := cadence.NewString("mintedBlock")
-		blockNumberTrait := traits.Values[1].(cadence.Struct)
-		assert.Equal(t, blockNumberName, blockNumberTrait.Fields[0])
-		assert.Equal(t, cadence.NewUInt64(13), blockNumberTrait.Fields[1])
-		assert.Equal(t, cadence.NewOptional(nil), blockNumberTrait.Fields[2])
-		assert.Equal(t, cadence.NewOptional(nil), blockNumberTrait.Fields[3])
-
-		mintedTimeName, _ := cadence.NewString("mintedTime")
-		mintedTimeDisplayType, _ := cadence.NewString("Date")
-		mintedTimeTrait := traits.Values[2].(cadence.Struct)
-		assert.Equal(t, mintedTimeName, mintedTimeTrait.Fields[0])
-		assert.Equal(t, cadence.NewOptional(mintedTimeDisplayType), mintedTimeTrait.Fields[2])
-
-		fooName, _ := cadence.NewString("foo")
-		fooValue, _ := cadence.NewString("bar")
-		fooTrait := traits.Values[3].(cadence.Struct)
-		fooRarityOptional := fooTrait.Fields[3].(cadence.Optional)
-		fooRarity := fooRarityOptional.Value.(cadence.Struct)
-		rarityDescription, _ := cadence.NewString("Common")
-		assert.Equal(t, fooName, fooTrait.Fields[0])
-		assert.Equal(t, cadence.NewOptional(fooValue), fooTrait.Fields[1])
-		fooRarityScore := fooRarity.Fields[0].(cadence.Optional).Value
-		score, _ := cadence.NewUFix64("10.0")
-		assert.Equal(t, fooRarityScore, score)
-		fooRarityMax := fooRarity.Fields[1].(cadence.Optional).Value
-		max, _ := cadence.NewUFix64("100.0")
-		assert.Equal(t, max, fooRarityMax)
-		assert.Equal(t, fooRarity.Fields[2], cadence.NewOptional(rarityDescription))
+		expectedMetadata := `
+{
+  "collectionBannerImage": "https://assets.website-files.com/5f6294c0c7a8cdd643b1c820/5f6294c0c7a8cda55cb1c936_Flow_Wordmark.svg",
+  "collectionDescription": "This collection is used as an example to help you develop your next Flow NFT.",
+  "collectionExternalURL": "https://example-nft.onflow.org",
+  "collectionName": "The Example Collection",
+  "collectionProviderLinkedType": "\u0026A.f8d6e0586b0a20c7.ExampleNFT.Collection{A.f8d6e0586b0a20c7.ExampleNFT.ExampleNFTCollectionPublic,A.f8d6e0586b0a20c7.NonFungibleToken.CollectionPublic,A.f8d6e0586b0a20c7.NonFungibleToken.Provider,A.f8d6e0586b0a20c7.MetadataViews.ResolverCollection}",
+  "collectionProviderPath": "/private/exampleNFTCollection",
+  "collectionPublic": "\u0026A.f8d6e0586b0a20c7.ExampleNFT.Collection{A.f8d6e0586b0a20c7.ExampleNFT.ExampleNFTCollectionPublic}",
+  "collectionPublicLinkedType": "\u0026A.f8d6e0586b0a20c7.ExampleNFT.Collection{A.f8d6e0586b0a20c7.ExampleNFT.ExampleNFTCollectionPublic,A.f8d6e0586b0a20c7.NonFungibleToken.CollectionPublic,A.f8d6e0586b0a20c7.NonFungibleToken.Receiver,A.f8d6e0586b0a20c7.MetadataViews.ResolverCollection}",
+  "collectionPublicPath": "/public/exampleNFTCollection",
+  "collectionSocials": {
+   "twitter": "https://twitter.com/flow_blockchain"
+  },
+  "collectionSquareImage": "https://assets.website-files.com/5f6294c0c7a8cdd643b1c820/5f6294c0c7a8cda55cb1c936_Flow_Wordmark.svg",
+  "collectionStoragePath": "/storage/exampleNFTCollection",
+  "description": "This is an example NFT",
+  "edition": {
+   "name": "Example NFT Edition",
+   "number": 0
+  },
+  "externalURL": "https://example-nft.onflow.org/0",
+  "name": "Example NFT 0",
+  "owner": "0x01cf0e2f2f715450",
+  "royalties": [
+   {
+    "cut": 0.25,
+    "description": "minter",
+    "receiver": "Capability\u003c\u0026AnyResource{A.ee82856bf20e2aa6.FungibleToken.Receiver}\u003e(address: 0x01cf0e2f2f715450, path: /public/GenericFTReceiver)"
+   },
+   {
+    "cut": 0.4,
+    "description": "creator",
+    "receiver": "Capability\u003c\u0026AnyResource{A.ee82856bf20e2aa6.FungibleToken.Receiver}\u003e(address: 0x179b6b1cb6755e31, path: /public/GenericFTReceiver)"
+   }
+  ],
+  "serialNumber": 0,
+  "thumbnail": "example.jpeg",
+  "traits": {
+   "traits": [
+    {
+     "name": "minter",
+     "value": "0x01cf0e2f2f715450",
+     "rarity": {
+      "description": "",
+      "max": 0,
+      "score": 0
+     }
+    },
+    {
+     "name": "mintedBlock",
+     "value": 10,
+     "rarity": {
+      "description": "",
+      "max": 0,
+      "score": 0
+     }
+    },
+    {
+     "name": "mintedTime",
+     "value": "mock_time",
+     "displayType": "Date",
+     "rarity": {
+      "description": "",
+      "max": 0,
+      "score": 0
+     }
+    },
+    {
+     "name": "foo",
+     "value": "bar",
+     "rarity": {
+      "description": "Common",
+      "max": 100,
+      "score": 10
+     }
+    }
+   ]
+  },
+  "type": "A.f8d6e0586b0a20c7.ExampleNFT.NFT"
+ }`
+		assert.JSONEq(t, expectedMetadata, string(data))
 	})
 }
 
 func TestSetupCollectionFromNFTReference(t *testing.T) {
-	b, accountKeys := newTestSetup(t)
+	o, err := OverflowTesting(WithBasePath("../../.."), WithScriptFolderName("transactions/scripts"))
+	assert.NoError(t, err)
 
-	// Create a new account to setting up a new account
-	aAddress, _, aSigner := newAccountWithAddress(b, accountKeys)
+	setupAccount := o.TxFileNameFN("setup_account")
+	setupFlowRoyalty := o.TxFileNameFN("setup_account_to_receive_royalty", Arg("vaultPath", "/storage/flowTokenVault"))
+	setupAccount(SignProposeAndPayAs("alice")).AssertSuccess(t)
+	setupFlowRoyalty(SignProposeAndPayAs("alice")).AssertSuccess(t)
 
-	// Create new keys for the NFT contract account
-	// and deploy all the NFT contracts
-	exampleNFTAccountKey, exampleNFTSigner := accountKeys.NewWithSigner()
-	nftAddress, metadataAddress, exampleNFTAddress := deployNFTContracts(t, b, exampleNFTAccountKey)
+	mintNft := o.TxFileNameFN("mint_nft", SignProposeAndPayAsServiceAccount(),
+		Arg("name", "Example NFT 0"),
+		Arg("description", "This is an example NFT"),
+		Arg("thumbnail", "example.jpeg"),
+		Arg("cuts", "[0.25, 0.40]"),
+		Arg("royaltyDescriptions", `["minter","creator"]`),
+		Addresses("royaltyBeneficiaries", "alice", "alice"))
 
-	// Mint a single NFT with standard royalty cuts and metadata
-	mintExampleNFT(t, b,
-		accountKeys,
-		nftAddress, metadataAddress, exampleNFTAddress,
-		exampleNFTAccountKey,
-		exampleNFTSigner)
+	id := mintNft(Arg("recipient", "alice")).AssertSuccess(t).GetIdFromEvent("A.f8d6e0586b0a20c7.ExampleNFT.Deposit", "id")
 
 	t.Run("Should be able to setup an account using the NFTCollectionData metadata view of a referenced NFT", func(t *testing.T) {
-		// Ideally, the exampleNFTAddress would not be needed in order to perform the full setup, but it is required
-		// until the following issue is supported in cadence: https://github.com/onflow/cadence/issues/1617
-		script := templates.GenerateSetupAccountFromNftReferenceScript(nftAddress, exampleNFTAddress, metadataAddress)
-		tx := createTxWithTemplateAndAuthorizer(b, script, aAddress)
 
-		tx.AddArgument(cadence.NewAddress(exampleNFTAddress))
-		tx.AddArgument(cadence.Path{Domain: "public", Identifier: "exampleNFTCollection"})
-		tx.AddArgument(cadence.NewUInt64(0))
-
-		signAndSubmit(
-			t, b, tx,
-			[]flow.Address{
-				b.ServiceKey().Address,
-				aAddress,
-			},
-			[]crypto.Signer{
-				b.ServiceKey().Signer(),
-				aSigner,
-			},
-			false,
-		)
-
-		assertCollectionLength(t, b, nftAddress, exampleNFTAddress,
-			aAddress,
-			0,
-		)
+		o.Tx("setup_account_from_nft_reference",
+			SignProposeAndPayAs("bob"),
+			Arg("address", "alice"),
+			Arg("publicPath", cadence.Path{Domain: "public", Identifier: "exampleNFTCollection"}),
+			Arg("id", id),
+		).AssertSuccess(t)
 	})
+
+}
+
+type Metadata struct {
+	CollectionBannerImage        string `json:"collectionBannerImage"`
+	CollectionDescription        string `json:"collectionDescription"`
+	CollectionExternalURL        string `json:"collectionExternalURL"`
+	CollectionName               string `json:"collectionName"`
+	CollectionProviderLinkedType string `json:"collectionProviderLinkedType"`
+	CollectionProviderPath       string `json:"collectionProviderPath"`
+	CollectionPublic             string `json:"collectionPublic"`
+	CollectionPublicLinkedType   string `json:"collectionPublicLinkedType"`
+	CollectionPublicPath         string `json:"collectionPublicPath"`
+	CollectionSocials            struct {
+		Twitter string `json:"twitter"`
+	} `json:"collectionSocials"`
+	CollectionSquareImage string `json:"collectionSquareImage"`
+	CollectionStoragePath string `json:"collectionStoragePath"`
+	Description           string `json:"description"`
+	Edition               struct {
+		Name   string `json:"name"`
+		Number int    `json:"number"`
+	} `json:"edition"`
+	ExternalURL string `json:"externalURL"`
+	Name        string `json:"name"`
+	Owner       string `json:"owner"`
+	Royalties   []struct {
+		Cut         float64 `json:"cut"`
+		Description string  `json:"description"`
+		Receiver    string  `json:"receiver"`
+	} `json:"royalties"`
+	SerialNumber int    `json:"serialNumber"`
+	Thumbnail    string `json:"thumbnail"`
+	Traits       struct {
+		Traits []struct {
+			Name        string      `json:"name"`
+			Value       interface{} `json:"value"`
+			DisplayType string      `json:"displayType,omitempty"`
+			Rarity      struct {
+				Description string `json:"description"`
+				Max         int    `json:"max"`
+				Score       int    `json:"score"`
+			} `json:"rarity,omitempty"`
+		} `json:"traits"`
+	} `json:"traits"`
+	Type string `json:"type"`
 }
--- lib/go/test/nft_test.go
+++ lib/go/test/nft_test.go
@@ -1,6 +1,7 @@
 package test
 
 import (
+	"fmt"
 	"testing"
 
 	. "github.com/bjartek/overflow/overflow"
@@ -14,6 +15,8 @@ func TestNFT(t *testing.T) {
 
 	setupAccount := o.TxFileNameFN("setup_account")
 	setupFlowRoyalty := o.TxFileNameFN("setup_account_to_receive_royalty", Arg("vaultPath", "/storage/flowTokenVault"))
+
+	destroyNFT := o.TxFileNameFN("destroy_nft")
 
 	mintNft := o.TxFileNameFN("mint_nft", SignProposeAndPayAsServiceAccount(),
 		Arg("name", "Example NFT 0"),
@@ -30,11 +33,11 @@ func TestNFT(t *testing.T) {
 
 	t.Run("Should be able to mint a token", func(t *testing.T) {
 
-		setupAccount(SignProposeAndPayAs("alice")).Test(t).AssertSuccess()
-		setupAccount(SignProposeAndPayAs("bob")).Test(t).AssertSuccess()
-		setupFlowRoyalty(SignProposeAndPayAs("alice")).Test(t).AssertSuccess()
-		setupFlowRoyalty(SignProposeAndPayAs("bob")).Test(t).AssertSuccess()
-		mintNft(Arg("recipient", "alice")).Test(t).AssertSuccess()
+		setupAccount(SignProposeAndPayAs("alice")).AssertSuccess(t)
+		setupAccount(SignProposeAndPayAs("bob")).AssertSuccess(t)
+		setupFlowRoyalty(SignProposeAndPayAs("alice")).AssertSuccess(t)
+		setupFlowRoyalty(SignProposeAndPayAs("bob")).AssertSuccess(t)
+		mintNft(Arg("recipient", "alice")).AssertSuccess(t)
 
 		o.Script("borrow_nft", Arg("address", "alice"), Arg("id", "0")).GetAsInterface()
 		supply := o.Script("get_total_supply").GetAsJson()
@@ -52,66 +55,39 @@ func TestNFT(t *testing.T) {
 	t.Run("Shouldn't be able to withdraw an NFT that doesn't exist in a collection", func(t *testing.T) {
 
 		o.Tx("transfer_nft", SignProposeAndPayAs("alice"), Arg("recipient", "bob"), Arg("withdrawID", 4)).
-			Test(t).AssertFailure("missing NFT")
+			AssertFailure(t, "missing NFT")
 	})
 
 	// Transfer an NFT correctly
 	t.Run("Should be able to withdraw an NFT and deposit to another accounts collection", func(t *testing.T) {
-		setupAccount(SignProposeAndPayAs("alice")).Test(t).AssertSuccess()
-		setupAccount(SignProposeAndPayAs("bob")).Test(t).AssertSuccess()
-		setupFlowRoyalty(SignProposeAndPayAs("alice")).Test(t).AssertSuccess()
-		setupFlowRoyalty(SignProposeAndPayAs("bob")).Test(t).AssertSuccess()
-		id := mintNft(Arg("recipient", "alice")).Test(t).AssertSuccess().GetIdFromEvent("A.f8d6e0586b0a20c7.ExampleNFT.Deposit", "id")
+		setupAccount(SignProposeAndPayAs("alice")).AssertSuccess(t)
+		setupAccount(SignProposeAndPayAs("bob")).AssertSuccess(t)
+		setupFlowRoyalty(SignProposeAndPayAs("alice")).AssertSuccess(t)
+		setupFlowRoyalty(SignProposeAndPayAs("bob")).AssertSuccess(t)
+		nft := mintNft(Arg("recipient", "alice")).AssertSuccess(t)
 
-		o.Tx("transfer_nft", SignProposeAndPayAs("alice"), Arg("recipient", "bob"), Arg("withdrawID", id)).
-			Test(t).AssertSuccess()
+		//TODO: how is the best way to test events?
+		assert.Equal(t, fmt.Sprintf("%v", nft.Events), "map[A.f8d6e0586b0a20c7.ExampleNFT.Deposit:[map[id:1 to:0x01cf0e2f2f715450]]]")
+
+		id := nft.GetIdFromEvent("A.f8d6e0586b0a20c7.ExampleNFT.Deposit", "id")
+
+		o.Tx("transfer_nft", SignProposeAndPayAs("alice"), Arg("recipient", "bob"), Arg("withdrawID", id)).AssertSuccess(t)
 
 		result := o.Script("borrow_nft", Arg("address", "bob"), Arg("id", id))
 		assert.NoError(t, result.Err)
+	})
+
+	t.Run("Should be able to withdraw an NFT and destroy it, not reducing the supply", func(t *testing.T) {
+		setupAccount(SignProposeAndPayAs("alice")).AssertSuccess(t)
+		setupFlowRoyalty(SignProposeAndPayAs("alice")).AssertSuccess(t)
+		id := mintNft(Arg("recipient", "alice")).AssertSuccess(t).GetIdFromEvent("A.f8d6e0586b0a20c7.ExampleNFT.Deposit", "id")
+
+		destroyNFT(SignProposeAndPayAs("alice"), Arg("id", id)).AssertSuccess(t)
+		supply := o.Script("get_total_supply").GetAsJson()
+		assert.Equal(t, "3", supply)
+
+		collectionLength := o.Script("get_collection_length", Arg("address", "alice")).GetAsJson()
+		assert.Equal(t, "1", collectionLength)
 
 	})
-	/*
-
-		t.Run("Should be able to withdraw an NFT and destroy it, not reducing the supply", func(t *testing.T) {
-
-			// This transaction withdraws the specifed NFT from the authorizers account
-			// and calls `destroy NFT`
-			script := templates.GenerateDestroyNFTScript(nftAddress, exampleNFTAddress)
-
-			tx := createTxWithTemplateAndAuthorizer(b, script, joshAddress)
-
-			// Destroy the only NFT in the collection
-			tx.AddArgument(cadence.NewUInt64(0))
-
-			signAndSubmit(
-				t, b, tx,
-				[]flow.Address{
-					b.ServiceKey().Address,
-					joshAddress,
-				},
-				[]crypto.Signer{
-					b.ServiceKey().Signer(),
-					joshSigner,
-				},
-				false,
-			)
-
-			// Both collections should now be empty since the only NFT was destroyed
-
-			assertCollectionLength(t, b, nftAddress, exampleNFTAddress,
-				joshAddress,
-				0,
-			)
-
-			assertCollectionLength(t, b, nftAddress, exampleNFTAddress,
-				exampleNFTAddress,
-				0,
-			)
-
-			// The total Supply should not have decreased, because it is used to make new IDs
-			script = templates.GenerateGetTotalSupplyScript(nftAddress, exampleNFTAddress)
-			supply := executeScriptAndCheck(t, b, script, nil)
-			assert.Equal(t, cadence.NewUInt64(1), supply)
-		})
-	*/
 }
