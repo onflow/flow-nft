4f44c7d61b987a9b9ca3589d518fb2ce102d8bdb
Josh Hannan <hannanjoshua19@gmail.com>
2025-02-04 12:10:07 -0600

    update to new emulator

--- lib/go/test/nft_test.go
+++ lib/go/test/nft_test.go
@@ -5,8 +5,8 @@ import (
 	"testing"
 
 	"github.com/onflow/cadence"
+	"github.com/onflow/cadence/common"
 	jsoncdc "github.com/onflow/cadence/encoding/json"
-	"github.com/onflow/cadence/runtime/common"
 	"github.com/onflow/flow-go-sdk"
 	"github.com/onflow/flow-go-sdk/crypto"
 	"github.com/stretchr/testify/assert"
